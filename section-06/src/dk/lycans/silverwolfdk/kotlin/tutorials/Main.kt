ffa973e2a5dcf01f7ed5bfad275116ffcc7b4da5
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 22:46:20 +0100

    Watched video 53,54

---
+++ section-06/src/dk/lycans/silverwolfdk/kotlin/tutorials/Main.kt
@@ -0,0 +1,71 @@
+package dk.lycans.silverwolfdk.kotlin.tutorials
+
+fun main() {
+
+    val range = 1..5
+
+//    for (i in range) {
+//        println(i)
+//    }
+
+    val charRange = 'a'..'z'
+    val backwardsRange = 5.downTo(1)
+    val rangeStep3 = (3..15).step(3)
+
+    val str = "Hello"
+//    for (c in str) {
+//        println(c)
+//    }
+
+//    for (num in (1..25).reversed().step(4)) {
+//        println(num)
+//    }
+
+//    for (num in 1 until 10) {
+//        println(num)
+//    }
+//
+//    for ((index, num) in arrayListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).withIndex()) {
+//        println("$index: $num")
+//    }
+//
+//    val seasons = arrayOf("spring", "summer", "winter", "fall")
+//    for ((index, season) in seasons.withIndex()) {
+//        println("$index: $season")
+//    }
+//
+//    val notASeason = "whatever" !in seasons
+//    println("whatever is not a season? $notASeason")
+//
+//    seasons.forEach { println("$it") }
+//    seasons.forEachIndexed { index, value -> println("$value is season number $index") }
+//
+//    // Loop names: name@ -> name postfixed by @
+//    for (i in 1..3) {
+//        println("i == $i")
+//        jLoop@ for (j in 1..4) {
+//            println("j == $j")
+//            for (k in 5..10) {
+//                println("k == $k")
+//                if (k == 7)
+//                    break@jLoop
+//            }
+//        }
+//    }
+//
+
+//
+//    for(p in backwardsRange) {
+//        println(p)
+//    }
+//
+//    for (n in rangeStep3) {
+//        println(n)
+//    }
+//
+//    for (i in (5..50).step(5)) {
+//        println(i)
+//    }
+
+
+}
